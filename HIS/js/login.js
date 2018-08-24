d3d3783879a1ebbad18a9c6e492e45bef70ab1d2
Ginbing <1223137796gin@gmail.com>
2018-08-24 11:56:06 +0800

    完成前、后台登录界面

--- HIS/js/login.js
+++ HIS/js/login.js
@@ -86,21 +86,3 @@ function check() {
 	else
 		return true;
 }
-function ENLanguage() {
-	$A("title")[0].innerText = "Sign in";
-	$("his").innerHTML = "Hospital Information System";
-	$("his").style.cssText = "letter-spacing: initial; right: 0px;";
-	$("si").innerHTML = "Sign in";
-	$("username").placeholder = "User ID";
-	$("userpwd").placeholder = "Password";
-	$("submit").value = "Sign In";
-}
-function ZHLanguage() {
-	$A("title")[0].innerText = "登录";
-	$("his").innerHTML = "医院信息管理系统";
-	$("his").style.cssText = "letter-spacing: 20px; right: -20px; position: relative;";
-	$("si").innerHTML = "登录";
-	$("username").placeholder = "用户名";
-	$("userpwd").placeholder = "密码";
-	$("submit").value = "登录";
-}
