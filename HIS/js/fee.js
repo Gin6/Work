89489c553556c3196a400afaa57ec757729308d2
Ginbing <1223137796gin@gmail.com>
2018-08-28 23:45:11 +0800

    pay

---
+++ HIS/js/fee.js
@@ -0,0 +1,17 @@
+function $B(id) {
+	return document.getElementById(id);
+}
+function mov_1() {
+	$B('auther_head').style.cssText = "width: 29px;top: 10px;right: 95px;";
+	$B('username_1').style.cssText = "right: 30px; z-index: 2;";
+}
+function mou_1() {
+	$B('auther_head').style.cssText = "width: 40px;top: 5px;right: 55px;";
+	$B('username_1').style.cssText = "right: 75px; z-index: 1;";
+}
+function pay() {
+	$B('paybox').style.cssText = "top: 0%;";
+}
+function pay_back() {
+	$B('paybox').style.cssText = "top: 100%; transition: .3s;";
+}
