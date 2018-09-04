9d0a8450c20f7bece23ad8179feef3adf48956aa
Ginbing <1223137796gin@gmail.com>
2018-09-04 11:32:23 +0800

    finish drugs.html

--- HIS/js/drugs.js
+++ HIS/js/drugs.js
@@ -2,18 +2,22 @@ var main = document.getElementById('main');
 main.style.height = $(window).height()-72+"px";
 $(document).ready(function (){
 	$('.main_content').mouseover(function (){
-		$('.main_right_bottom').css({"height": "5%", "border": "1px solid rgba(74, 74, 74, 0)",});
+		$('.main_right_bottom').css({"height": "5%",});
 		$('.main_right_top').css({"height": "61%",});
-		$('.drop_b').css({"z-index": "-1", "padding-top": "0%", "margin-top": "0%",});
+		$('.drop_b').css({"z-index": "-1", "padding-top": "0%",});
 		$('.submit_bar').css({"height": "100%",});
 		$('.submit_2').css({"margin-bottom": "0%", "left": "0px", "width": "100%", "height": "40px", "background-color": "#57a0d2cf",});
 		$('.submit_2').val("返回");
 		$('.button_1').css({"z-index": "3","background-color": "rgb(255, 138, 138)",});
+		$('.main_content').css({"box-shadow": "rgba(0, 0, 0, 0.43) -3px 3px 5px", "left": "3px",});
+	});
+	$('.main_content').mouseout(function (){
+		$('.main_content').css({"box-shadow": "rgba(0, 0, 0, 0.43) 0px 0px 0px", "left": "0%",});
 	});
 	$('.main_a').mouseout(function (){
-		$('.main_right_bottom').css({"height": "70%", "border": "1px solid rgba(74, 74, 74, 0.79)",});
+		$('.main_right_bottom').css({"height": "70%",});
 		$('.main_right_top').css({"height": "0%",});
-		$('.drop_b').css({"z-index": "3", "padding-top": "3%", "margin-top": "1%",});
+		$('.drop_b').css({"z-index": "3", "padding-top": "3%",});
 		$('.submit_bar').css({"height": "15%",});
 		$('.submit_2').css({"margin-bottom": "5%", "left": "10%", "width": "80%", "height": "35px", "background-color": "rgb(255, 138, 138)",});
 		$('.submit_2').val("添加");
@@ -23,18 +27,18 @@ $(document).ready(function (){
 		$('.drop_t_i').css({"border": "none", "border-bottom": "1px solid rgb(120, 120, 120)", "margin-top": "4%", "border-radius": "0px",});
 	});
 	$('.button_1').mouseover(function (){
-		$('.main_right_bottom').css({"height": "5%", "border": "1px solid rgba(74, 74, 74, 0)",});
+		$('.main_right_bottom').css({"height": "5%",});
 		$('.main_right_top').css({"height": "61%",});
-		$('.drop_b').css({"z-index": "-1", "padding-top": "0%", "margin-top": "0%",});
+		$('.drop_b').css({"z-index": "-1", "padding-top": "0%",});
 		$('.submit_bar').css({"height": "100%",});
 		$('.submit_2').css({"margin-bottom": "0%", "left": "0px", "width": "100%", "height": "40px", "background-color": "#57a0d2cf",});
 		$('.submit_2').val("返回");
 		$('.button_1').css({"background-color": "#f67272",});
 	});
 	$('.submit_3').mouseover(function (){
-		$('.main_right_bottom').css({"height": "5%", "border": "1px solid rgba(74, 74, 74, 0)",});
+		$('.main_right_bottom').css({"height": "5%",});
 		$('.main_right_top').css({"height": "61%",});
-		$('.drop_b').css({"z-index": "-1", "padding-top": "0%", "margin-top": "0%",});
+		$('.drop_b').css({"z-index": "-1", "padding-top": "0%",});
 		$('.submit_bar').css({"height": "100%",});
 		$('.submit_2').css({"margin-bottom": "0%", "left": "0px", "width": "100%", "height": "40px", "background-color": "#57a0d2cf",});
 		$('.submit_2').val("返回");
@@ -43,9 +47,9 @@ $(document).ready(function (){
 		$('.drop_t_i').css({"border": "1px solid rgb(120, 120, 120)", "height": "40%", "margin-top": "4%", "border-radius": "3px",});
 	});
 	$('.drop_t').mouseover(function (){
-		$('.main_right_bottom').css({"height": "5%", "border": "1px solid rgba(74, 74, 74, 0)",});
+		$('.main_right_bottom').css({"height": "5%",});
 		$('.main_right_top').css({"height": "61%",});
-		$('.drop_b').css({"z-index": "-1", "padding-top": "0%", "margin-top": "0%",});
+		$('.drop_b').css({"z-index": "-1", "padding-top": "0%",});
 		$('.submit_bar').css({"height": "100%",});
 		$('.submit_2').css({"margin-bottom": "0%", "left": "0px", "width": "100%", "height": "40px", "background-color": "#57a0d2cf",});
 		$('.submit_2').val("返回");
