56a6a8f8389b73fbb51ab50e0dd35a22a71202a0
Ginbing <1223137796gin@gmail.com>
2018-09-06 13:12:36 +0800

    one commit

--- HIS/js/department.js
+++ HIS/js/department.js
@@ -2,19 +2,23 @@ var main = document.getElementById('main');
 main.style.height = $(window).height()-72+"px";
 $('.module').css('height',($(window).height()-72)*.255+"px");
 $(document).ready(function (){
-	$('#module_bar').mousedown(function (){
+	$('.modif').mousedown(function (){
 		timeout = setTimeout(function() {
-			$('#module').css({"background-color": "#f5c4c4", "user-select": "none",});
-			$('.md').css({"z-index": "1",});
+			$('.modif').css({"background-color": "rgba(0, 0, 0, 0.05)",});
 		}, 100);
 	});
-	$('#module_bar').mouseup(function (){
-		clearTimeout(timeout);
-		$('#module').css({"background-color": "#f5f5f5",});
-		$('.md').css({"z-index": "-1",});
+	$('.delete').mousedown(function (){
+		timeout = setTimeout(function() {
+			$('.delete').css({"background-color": "rgba(0, 0, 0, 0.05)",});
+			setTimeout(function() {
+				$('.delete_a')[0].click();
+			}, 2000);
+		}, 100);
 	});
-	$('#module_bar').mouseout(function (){
-		$('#module').css({"background-color": "#f5f5f5",});
-		$('.md').css({"z-index": "-1",});
+	$('.md').mouseup(function (){
+		clearTimeout(timeout);
+		$('.md').css({"background-color": "rgba(0, 0, 0, 0)",});	});
+	$('.md').mouseout(function (){
+		$('.md').css({"background-color": "rgba(0, 0, 0, 0)",});
 	});
 });
